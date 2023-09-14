640630e148169275d6dcb7952276a23343f4a60b
hybridx <hybridx18@gmail.com>
2023-09-15 01:59:08 +0530

    Added public path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3000/",
+    publicPath: "/",
   },
 
   resolve: {
